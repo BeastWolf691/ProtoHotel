a2459a2c346fc5dd64b408cea32da4d415596900
Steven Riviere <sriver@hotmail.fr>
2024-04-29 10:37:19 +0200

    test

--- src/View/Templates/Header_Home.php
+++ src/View/Templates/Header_Home.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><!--lien de bootstrap-->
-    <link href="../../../styleProject.css" rel="stylesheet" type="text/css" media="all"/>
-</head>
 
-<body>
     <div class="banniere">
         <h1 class="titre">GT Hôtel</h1>
         <ul class="navbar navbar-expand-xl justify-content-end">
@@ -34,5 +24,3 @@
             </form>
         </ul>
     </div>
-    </body>
-    </html>
