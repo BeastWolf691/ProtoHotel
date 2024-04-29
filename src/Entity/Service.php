68c031ba38c5f34a88f1c46829f0f2166c638f6b
Steven Riviere <sriver@hotmail.fr>
2024-04-29 14:50:58 +0200

    test

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -15,14 +15,10 @@ $services = [
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Services de l'Hôtel</title>
-    <!-- Intégration de Bootstrap pour le style -->
-    <link href="path_to_bootstrap.css" rel="stylesheet">
-</head>
+<link href="../../src/styleProject.css" rel="stylesheet" type="text/css" media="all" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><!--lien de bootstrap-->
+
 <body>
 
 <div class="container">
