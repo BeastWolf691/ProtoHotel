13ae8b9ac544f9cd41cd5619c7c548750a755a08
Steven Riviere <sriver@hotmail.fr>
2024-04-29 13:56:15 +0200

    test

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\ORMSetup;
  
 // Create a simple "default" Doctrine ORM configuration for Attributes
 $config = ORMSetup::createAttributeMetadataConfiguration(
-    paths: array(__DIR__."/src/classes"),
+    paths: array(__DIR__."/src/Views"),
     isDevMode: true,
 );
  
