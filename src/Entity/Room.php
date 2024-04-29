afaf12f1ca8ac5c7f81b3b803e44c5905f42a657
Steven Riviere <sriver@hotmail.fr>
2024-04-29 11:27:03 +0200

    test

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -1,6 +1,6 @@
-<?php include 'header.php'; ?>
+
 <!-- Ici, vous afficherez les chambres avec une boucle foreach -->
 <?php foreach ($rooms as $room): ?>
   <!-- Afficher les informations de la chambre, inclure une image et le prix -->
 <?php endforeach; ?>
-<?php include 'footer.php'; ?>
+
