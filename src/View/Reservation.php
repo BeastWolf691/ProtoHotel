3bd66a0fe7693b8c058c8b2bd78c17552cc076fc
Steven Riviere <sriver@hotmail.fr>
2024-04-29 14:43:10 +0200

    test

--- src/View/Reservation.php
+++ src/View/Reservation.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 ?>
 
-<link href="../View/Reservation.php" rel="stylesheet" type="text/css" media="all" />
+<link href="../../src/styleProject.css" rel="stylesheet" type="text/css" media="all" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><!--lien de bootstrap-->
 
