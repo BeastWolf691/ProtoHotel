99be947e84d124b0bda31b0a462334cbe6a4b3be
Steven Riviere <sriver@hotmail.fr>
2024-05-01 13:49:32 +0200

    test

--- gthotel.sql
+++ gthotel.sql
@@ -1,17 +1,15 @@
--- Active: 1713252765707@@127.0.0.1@3306@gthotel
 CREATE TABLE rooms (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     description TEXT,
     price DECIMAL(10, 2),
-    image_path VARCHAR(255)
+    available INT
 );
 
-INSERT INTO rooms (name, description, price, image_path) VALUES
-('Chambre Standard', 'Une belle chambre pour deux personnes.', 100.00, 'images/rooms/chb 2 lits simples 109.jpg'),
-('Chambre Luxe', 'Spacieuse et luxueuse, avec vue sur la mer.', 200.00, 'images/rooms/chb 2 lits simples 109.jpg'),
-('Suite Royale', 'Notre suite la plus luxueuse.', 500.00, 'images/rooms/chb 2 lits simples 111.jpg');
-
+INSERT INTO rooms (name, description, price, image, available) VALUES
+('Chambre Standard', 'Une belle chambre pour deux personnes.', 100.00, 1),
+('Chambre Luxe', 'Spacieuse et luxueuse, avec vue sur la mer.', 200.00, 0),
+('Suite Royale', 'Notre suite la plus luxueuse.', 500.00, 1);
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,17 +20,12 @@ CREATE TABLE users (
     address VARCHAR(255)
 );
 
-INSERT INTO users (username, email, fullname, password, address) VALUES('Sophie','sophieduval@gmail.com','duval',458781,'45rue du desespoir');
+INSERT INTO users (username, email, fullname, password, address) VALUES ('Sophie', 'sophieduval@gmail.com', 'duval', 'hashed_password', '45 rue du désespoir');
 
 CREATE TABLE hotels (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL
 );
+
 INSERT INTO hotels (name, description) VALUES ('Hôtel Plaza Athénée', 'Situé au cœur de Paris, cet hôtel offre une expérience luxueuse avec une vue magnifique sur la Tour Eiffel.');
--- Répétez pour les autres hôtels
-CREATE TABLE hotels (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL
-);
